c07e8e8ad03a4955ec169c000bcdef43132d8d13
sleepychaman <ryabicho@student.42.fr>
2022-03-13 13:03:53 +0100

    ad grpc

--- internal/world/entity/character.go
+++ internal/world/entity/character.go
@@ -1,3 +1,24 @@
 package entity
 
-type Character struct{}
+import "google.golang.org/grpc"
+
+type Character int
+
+func (c *Character) Add(payload string, reply *string) error {
+	test := "gg"
+	reply = &test
+	return nil
+}
+
+var Greeter_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "Character.add",
+	HandlerType: (*GreeterServer)(nil),
+	Methods: []grpc.MethodDesc{
+		{
+			MethodName: "SayHello",
+			Handler:    _Greeter_SayHello_Handler,
+		},
+	},
+	Streams:  []grpc.StreamDesc{},
+	Metadata: "examples/helloworld/helloworld/helloworld.proto",
+}
