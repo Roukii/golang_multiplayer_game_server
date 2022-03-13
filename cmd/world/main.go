4440c60328264169a768218984e5d0aa690210ba
Alexandre Ablon <ablonalexandre@gmail.com>
2022-03-13 03:40:36 +0100

    dummy rpc test

--- cmd/world/main.go
+++ cmd/world/main.go
@@ -1,1 +1,19 @@
-package world
+package main
+
+import (
+	"log"
+
+	"github.com/Roukii/pock_multiplayer/config"
+	"github.com/Roukii/pock_multiplayer/internal/world"
+)
+
+func main() {
+	// Configuration
+	err := config.LoadConfig("./", "config")
+	if err != nil {
+		log.Fatalf("Config error: %s", err)
+	}
+
+	// Run
+	world.Run()
+}
--- internal/world/world.go
+++ internal/world/world.go
@@ -1,4 +1,19 @@
 package world
 
+import (
+	"fmt"
+	"net/rpc"
+)
+
+
+func (c *Character) Add(payload string, reply *string) error {
+	test := "gg"
+	reply = &test
+	return nil
+}
+
 func Run() {
+	if err := rpc.Register(&Character{}); err != nil {
+		fmt.Println(err)
+	}
 }
